af3987ccaa9c13f5e2d4fb17c001a82bfc66d52a
LarePVL <laurivir@gmail.com>
2019-08-03 19:46:29 +0300

    Enemy test

--- src/enemy.py
+++ src/enemy.py
@@ -1,14 +1,13 @@
 from entity import Entity
 
 class Enemy(Entity):
-    def __init__(self, ai, **kwargs):
-        super().__init__(**kwarks)
+    def __init__(self, ai, sprite, position, **kwargs):
+        super().__init__(sprite, position, **kwarks)
         self.ai = ai
 
     def tick(self, engine):
         if super().tick(engine):
             return None
         self.ai(engine)
-        
-        self.gravity(engine)
 
+        self.gravity(engine)
