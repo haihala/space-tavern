a0d3ced8f724d96a591ef853b0f9e88cb276ca7e
LarePVL <laurivir@gmail.com>
2019-08-04 08:08:26 +0300

    Player move sound update

--- src/player.py
+++ src/player.py
@@ -49,17 +49,17 @@ class Player(Entity):
                 if self._sprite != SPRITES["player_walk"]:
                     self.sprite_offset = 0
                 self._sprite = SPRITES["player_walk"]
-                self.move(engine, direction=[-1, 0])
                 SOUNDS["player_move"].play()
                 engine.camera_shake(1)
+                self.move(engine, direction=[-1, 0])
                 moved = True
             elif action == "right":
                 if self._sprite != SPRITES["player_walk"]:
                     self.sprite_offset = 0
                 self._sprite = SPRITES["player_walk"]
-                self.move(engine, direction=[1, 0])
                 SOUNDS["player_move"].play()
                 engine.camera_shake(1)
+                self.move(engine, direction=[1, 0])
                 moved = True
             elif action == "down":
                 self._sprite = SPRITES["player_idle"]
